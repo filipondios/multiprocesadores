3f24ed29cdd729a8237c305cb2910a418a55a5c6
dpv927 <dpv927@inlumine.ual.es>
2023-10-03 19:15:12 +0200

    Terminar mandel

--- Mandel/Mandel.c
+++ Mandel/Mandel.c
@@ -4,7 +4,7 @@
 #include "argshand.h"
 #include "utils.h"
 
-#define PRINT 1
+#define PRINT 0
 
 
 
@@ -52,17 +52,17 @@ void NIterToRGB(int NIter, char * pRed, char *pGreen, char *pBlue)
 /*---------------------------------------------------------------------------*/
 int main(int argc, char **argv)
 {
- int     MaxNIter;			//Max number of Iterations.
- int 	 NIter; 			//Number of iterations.
- int     Rows, Cols;		//Rows and Cols of the Image.
- double  MinX, MinY;		//Min x and y of the mandelbrot window.
- double  SizeX, SizeY; 		//Size in x and y of the Mandelbrot window.
- char ** ppGray; 			//Gray tone of pixels.
- char ** ppRed, ** ppGreen, ** ppBlue;  //RGB matrices (0..255)
- char *  FileName;
- FILE *  FOut;
- char    Command[256];
- char    InGray; 			//If the output is in gray 1=True or colored 0=False.
+ int      MaxNIter;			//Max number of Iterations.
+ int 	    NIter; 			//Number of iterations.
+ int      Rows, Cols;		//Rows and Cols of the Image.
+ double   MinX, MinY;		//Min x and y of the mandelbrot window.
+ double   SizeX, SizeY; 		//Size in x and y of the Mandelbrot window.
+ char **  ppGray; 			//Gray tone of pixels.
+ char **  ppRed, ** ppGreen, ** ppBlue;  //RGB matrices (0..255)
+ char *   FileName;
+ FILE *   FOut;
+ char     Command[256];
+ int      InGray; 			//If the output is in gray 1=True or colored 0=False.
 							//True and False are defined in util.h
  double  IncX, IncY; 		//Pixes width in x and y.
 
@@ -175,83 +175,84 @@ int main(int argc, char **argv)
  printf("InGray=%d, Max.N.iterations=%d,  Output=%s.\n",InGray, MaxNIter, FileName);
  #endif
 
- //InGray si la imégen en tonos de gris y !Ingray si el color.
- if (InGray)
-    {
+  //InGray si la imégen en tonos de gris y !Ingray si el color.
+  if (InGray) {
      //GetMem for ppRed, ppGreen and ppBlue. It put data to zeroes
      ppGray=(char **) GetMem2D (Rows,Cols,sizeof(char),"Main:ppGray");
+  } else {
+    //GetMem for ppRed, ppGreen and ppBlue. It put data to zeroes
+    //TODO: get memroy for the RGB matrices.
+    ppRed=(char **) GetMem2D (Rows,Cols,sizeof(char),"Main:ppRed");
+    ppGreen=(char **) GetMem2D (Rows,Cols,sizeof(char),"Main:ppGreen");
+    ppBlue=(char **) GetMem2D (Rows,Cols,sizeof(char),"Main:ppBlue");
+  }
+
+  IncX=SizeX/(double)Cols;
+  IncY=SizeY/(double)Rows;
+
+  //Number of iterations "NIter" per pixel:
+  for (int i=0;i<Rows;i++) {
+    for (int j=0;j<Cols;j++) {
+      zi=ci=MinY+i*IncY;
+      zr=cr=MinX+j*IncX;
+      zrs=zis=0.0;
+      NIter=0;
+
+      while (zrs+zis<=4.0 && NIter<=MaxNIter) {
+        zrs=zr*zr;
+        zis=zi*zi;
+        zi=2.0*zr*zi+ci;
+        zr=zrs-zis+cr;
+        NIter++;
+      }
+      if (InGray){
+        ppGray[i][j]=NIterToGray(NIter,MaxNIter);
+      } else {
+        //TODO: update RGB matrices of pixel [i][i] using NIterToRGB()
+        NIterToRGB(NIter, &ppRed[i][j], &ppGreen[i][j], &ppBlue[i][j]);
+      }
     }
-
- if (!InGray)
-    {
-     //GetMem for ppRed, ppGreen and ppBlue. It put data to zeroes
-     //TODO: get memroy for the RGB matrices.
-    }
-
- IncX=SizeX/(double)Cols;
- IncY=SizeY/(double)Rows;
-
- //Number of iterations "NIter" per pixel:
- for (int i=0;i<Rows;i++)
-     for (int j=0;j<Cols;j++)
-         {
-          zi=ci=MinY+i*IncY;
-          zr=cr=MinX+j*IncX;
-          zrs=zis=0.0;
-          NIter=0;
-          while (zrs+zis<=4.0 && NIter<=MaxNIter)
-                {
-                 zrs=zr*zr;
-                 zis=zi*zi;
-                 zi=2.0*zr*zi+ci;
-                 zr=zrs-zis+cr;
-                 NIter++;
-                 }
-          if (InGray)
-             ppGray[i][j]=NIterToGray(NIter,MaxNIter);
+  }
  
-          if (!InGray)
-             {
-              //TODO: update RGB matrices of pixel [i][i] using NIterToRGB()
-             }
-
-         }
+  //Write to the output file
+  FOut=OpenFile(FileName,"wb");
  
- //Write to the output file
- FOut=OpenFile(FileName,"wb");
- 
- if (InGray)
+  if (InGray) {
+    printf("gris");
     for (int i=0;i<Rows;i++) 
-        fwrite(ppGray[i],sizeof(char),(size_t)Cols,FOut);
-
- if (!InGray)
-    {
-     //TODO: Save RGB colors to a raw image file.
+      fwrite(ppGray[i],sizeof(char),(size_t)Cols,FOut);
+  } else {
+    //TODO: Save RGB colors to a raw image file.
+    for (int i=0;i<Rows;i++) {
+      for (int j=0; j<Cols; j++) {
+        fwrite(&ppRed[i][j],sizeof(char),1,FOut);
+        fwrite(&ppGreen[i][j],sizeof(char),1,FOut);
+        fwrite(&ppBlue[i][j],sizeof(char),1,FOut);
+      }
     }
+  }
 
  CloseFile(FOut);
 
- if (InGray)
-    {
+ if (InGray) {
      //Free memory for ppBlue, ppGreen and ppRed.
      Free2D((void **)ppGray, Rows);
-    }
-
- if (!InGray)
-    {
-     //TODO: Free memory of RGB matrices. 
-    }
+  } else {
+    //TODO: Free memory of RGB matrices. 
+    Free2D((void **)ppRed, Rows);
+    Free2D((void **)ppGreen, Rows);
+    Free2D((void **)ppBlue, Rows);
+  }
 
  //rawtopgm and pnmtopng are in netpbm package
- if (InGray)
+  if (InGray) {
     sprintf(Command,"rawtopgm %d %d %s | pnmtopng > %s.png", 
                     Cols,Rows, FileName, FileName);
- 
-//rawtoppm and pnmtopng are in netpbm package 
- if (!InGray)
-    {
-     //TODO: Generate the string with the command to move from RGB raw to png.
-    }
+  } else {
+    //TODO: Generate the string with the command to move from RGB raw to png.
+    sprintf(Command,"rawtoppm %d %d %s | pnmtopng > %s.png", 
+                    Cols,Rows, FileName, FileName);
+  }
 
  #if (PRINT==1)
  printf("Command=%s\n",Command);
@@ -259,7 +260,6 @@ int main(int argc, char **argv)
   
  system(Command);
 // eog from gnome visualize the image when it change
-
  return 0;
 }
 
